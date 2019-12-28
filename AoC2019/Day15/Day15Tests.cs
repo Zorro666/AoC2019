70425dbaf6fb714a9de5d304506eda1ce7f2f416
Jake Turner <jake@unity3d.com>
2019-12-28 17:37:15 +0000

    Day15 Start

--- AoC2019/Day15/Day15Tests.cs
+++ AoC2019/Day15/Day15Tests.cs
@@ -6,7 +6,7 @@ namespace Day15
     public class Tests
     {
         [Test]
-        public void Day11Tests(string parent, string node)
+        public void Day15Tests(string parent, string node)
         {
             Assert.Fail();
         }
