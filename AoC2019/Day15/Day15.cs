cd2711c2f6268ba8f3a1a9c39e0e4e49c3252b78
Jake Turner <jake@unity3d.com>
2019-12-30 07:35:31 +0000

    Day 15 Part 1 is done - start of part 2

--- AoC2019/Day15/Day15.cs
+++ AoC2019/Day15/Day15.cs
@@ -72,21 +72,337 @@ Now, because of the reply of 2, you know you've found the oxygen system! In this
 
 What is the fewest number of movement commands required to move the repair droid from its starting position to the location of the oxygen system?
 
+--- Part Two ---
+
+You quickly repair the oxygen system; oxygen gradually fills the area.
+
+Oxygen starts in the location containing the repaired oxygen system. It takes one minute for oxygen to spread to all open locations that are adjacent to a location that already contains oxygen. Diagonal locations are not adjacent.
+
+In the example above, suppose you've used the droid to explore the area fully and have the following map (where locations that currently contain oxygen are marked O):
+
+ ##   
+#..## 
+#.#..#
+#.O.# 
+ ###  
+Initially, the only location which contains oxygen is the location of the repaired oxygen system. However, after one minute, the oxygen spreads to all open (.) locations that are adjacent to a location containing oxygen:
+
+ ##   
+#..## 
+#.#..#
+#OOO# 
+ ###  
+After a total of two minutes, the map looks like this:
+
+ ##   
+#..## 
+#O#O.#
+#OOO# 
+ ###  
+After a total of three minutes:
+
+ ##   
+#O.## 
+#O#OO#
+#OOO# 
+ ###  
+And finally, the whole region is full of oxygen after a total of four minutes:
+
+ ##   
+#OO## 
+#O#OO#
+#OOO# 
+ ###  
+So, in this example, all locations contain oxygen after 4 minutes.
+
+Use the repair droid to get a complete map of the area. How many minutes will it take to fill with oxygen?
+
 */
 
 namespace Day15
 {
     class Program
     {
+        static readonly int MAX_MAP_SIZE = 1024;
+        static readonly int sStartX = MAX_MAP_SIZE / 2;
+        static readonly int sStartY = MAX_MAP_SIZE / 2;
+        static readonly int[,] sMap = new int[MAX_MAP_SIZE, MAX_MAP_SIZE];
+        static readonly int[,] sMapExplored = new int[MAX_MAP_SIZE, MAX_MAP_SIZE];
+        static int sDistance = 0;
+        static int sNumberOfSteps = 0;
+        static Random sRandom = new Random();
+        static IntProgram sIntProgram = new IntProgram();
+        enum Direction
+        {
+            North = 1,
+            South = 2,
+            West = 3,
+            East = 4
+        };
+
         private Program(string inputFile, bool part1)
         {
-            var elements = ReadProgram(inputFile);
+            sIntProgram.LoadProgram(inputFile);
+            if (part1)
+            {
+                var result = RunProgram();
+                Console.WriteLine($"Day15 : Result1 {result}");
+                if (result != 250)
+                {
+                    throw new InvalidProgramException($"Part1 result has been broken {result}");
+                }
+            }
+            else
+            {
+                var result = RunProgram();
+                Console.WriteLine($"Day15 : Result2 {result}");
+            }
         }
 
-        private string[] ReadProgram(string inputFile)
+        static public int RunProgram()
         {
-            var elements = File.ReadAllLines(inputFile);
-            return elements;
+            sMap[sStartX, sStartY] = 0;
+            int posX = sStartX;
+            int posY = sStartY;
+            bool halt = false;
+            while (!halt)
+            {
+                Search(Direction.North, posX, posY);
+                Search(Direction.South, posX, posY);
+                Search(Direction.West, posX, posY);
+                Search(Direction.East, posX, posY);
+                halt = true;
+            }
+            OutputMap(posX, posY);
+            return sDistance;
+        }
+
+        static bool MoveRobot(Direction direction, int currentX, int currentY)
+        {
+            (int newX, int newY) = GetNewPos(direction, currentX, currentY);
+            Int64 input = (Int64)direction;
+            var output = GetOutput(input);
+            bool moved = true;
+            // Clear
+            if (output == 1)
+            {
+                sMap[newX, newY] = 1;
+            }
+            // Oxygen
+            else if (output == 2)
+            {
+                sMap[newX, newY] = 2;
+            }
+            // Wall
+            else if (output == 0)
+            {
+                sMap[newX, newY] = 3;
+                newX = currentX;
+                newY = currentY;
+                moved = false;
+            }
+            else
+            {
+                throw new InvalidDataException($"Invalid output {output}");
+            }
+            OutputMap(newX, newY);
+            return moved;
+        }
+
+        static bool Search(Direction direction, int currentX, int currentY)
+        {
+            (int newX, int newY) = GetNewPos(direction, currentX, currentY);
+            bool moved = MoveRobot(direction, currentX, currentY);
+            if (!moved)
+            {
+                return false;
+            }
+
+            ++sNumberOfSteps;
+            // Oxygen
+            if (sMap[newX, newY] == 2)
+            {
+                sDistance = sNumberOfSteps;
+            }
+
+            // Valid move and already visited
+            if (sMapExplored[newX, newY] == 1)
+            {
+                MoveBack(direction, newX, newY);
+                --sNumberOfSteps;
+                return false;
+            }
+            // Valid moved and unknown map location
+            sMapExplored[newX, newY] = 1;
+
+            if (Search(Direction.North, newX, newY))
+            {
+                return true;
+            }
+            if (Search(Direction.South, newX, newY))
+            {
+                return true;
+            }
+            if (Search(Direction.West, newX, newY))
+            {
+                return true;
+            }
+            if (Search(Direction.East, newX, newY))
+            {
+                return true;
+            }
+
+            --sNumberOfSteps;
+            MoveBack(direction, newX, newY);
+            return false;
+        }
+
+        static void MoveBack(Direction direction, int currentX, int currentY)
+        {
+            if (direction == Direction.North)
+            {
+                MoveRobot(Direction.South, currentX, currentY);
+            }
+            else if (direction == Direction.South)
+            {
+                MoveRobot(Direction.North, currentX, currentY);
+            }
+            else if (direction == Direction.West)
+            {
+                MoveRobot(Direction.East, currentX, currentY);
+            }
+            else if (direction == Direction.East)
+            {
+                MoveRobot(Direction.West, currentX, currentY);
+            }
+            else
+            {
+                throw new InvalidDataException($"Invalid direction {direction}");
+            }
+        }
+
+        static (int, int) GetNewPos(Direction direction, int posX, int posY)
+        {
+            if (direction == Direction.North)
+            {
+                return (posX, posY - 1);
+            }
+            else if (direction == Direction.South)
+            {
+                return (posX, posY + 1);
+            }
+            else if (direction == Direction.West)
+            {
+                return (posX - 1, posY);
+            }
+            else if (direction == Direction.East)
+            {
+                return (posX + 1, posY);
+            }
+            else
+            {
+                throw new InvalidDataException($"Invalid input {direction}");
+            }
+        }
+
+        static Int64 GetOutput(Int64 input)
+        {
+            bool halt = false;
+            bool hasOutput = false;
+            var output = sIntProgram.RunProgram(input, ref halt, ref hasOutput);
+            if (halt && hasOutput)
+            {
+                throw new InvalidDataException($"halt and hasOutput can't be both true");
+            }
+            if (halt)
+            {
+                return -666;
+            }
+            if (!hasOutput)
+            {
+                throw new InvalidDataException($"hasOutput should be true");
+            }
+            return output;
+        }
+
+        static void OutputMap(int posX, int posY)
+        {
+            int minX = int.MaxValue;
+            int maxX = int.MinValue;
+            int minY = int.MaxValue;
+            int maxY = int.MinValue;
+            for (int y = 0; y < MAX_MAP_SIZE; ++y)
+            {
+                for (int x = 0; x < MAX_MAP_SIZE; ++x)
+                {
+                    if (sMap[x, y] != 0)
+                    {
+                        minX = Math.Min(minX, x);
+                        minY = Math.Min(minY, y);
+                        maxX = Math.Max(maxX, x);
+                        maxY = Math.Max(maxY, y);
+                    }
+                }
+            }
+            string topBottom = "|";
+            for (int x = minX; x <= maxX; ++x)
+            {
+                topBottom += "-";
+            }
+            topBottom += "|";
+            Console.WriteLine(topBottom);
+            for (int y = minY; y <= maxY; ++y)
+            {
+                string line = "|";
+                for (int x = minX; x <= maxX; ++x)
+                {
+                    if ((x == sStartX) && (y == sStartY))
+                    {
+                        line += 'S';
+                    }
+                    // Unknown
+                    else if (sMap[x, y] == 0)
+                    {
+                        line += ' ';
+                    }
+                    // Clear
+                    else if (sMap[x, y] == 1)
+                    {
+                        if ((x == posX) && (y == posY))
+                        {
+                            line += '*';
+                        }
+                        else
+                        {
+                            line += '.';
+                        }
+                    }
+                    // Oxygen
+                    else if (sMap[x, y] == 2)
+                    {
+                        if ((x == posX) && (y == posY))
+                        {
+                            line += '@';
+                        }
+                        else
+                        {
+                            line += 'E';
+                        }
+                    }
+                    // Wall
+                    else if (sMap[x, y] == 3)
+                    {
+                        line += '#';
+                    }
+                    else
+                    {
+                        throw new InvalidDataException($"Invalid map value {sMap[x, y]}");
+                    }
+                }
+                line += "|";
+                Console.WriteLine(line);
+            }
+            Console.WriteLine(topBottom);
         }
 
         public static void Run()
---
+++ AoC2019/IntProgram.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+
+struct IntProgram
+{
+    Int64[] mData;
+    Int64 mRelativeBase;
+    Int64 mPC;
+
+    public void LoadProgram(string inputFile)
+    {
+        var source = File.ReadAllText(inputFile);
+        CreateProgram(source);
+    }
+
+    public void CreateProgram(string source)
+    {
+        var sourceElements = source.Split(',');
+        mData = new Int64[sourceElements.Length];
+        var index = 0;
+        foreach (var element in sourceElements)
+        {
+            mData[index] = Int64.Parse(element);
+            index++;
+        }
+        mPC = 0;
+        mRelativeBase = 0;
+    }
+
+    public Int64 RunProgram(Int64 input, ref bool halt, ref bool hasOutput)
+    {
+        Int64 instruction = mData[mPC];
+        Int64 result = -666;
+        while (instruction != 99)
+        {
+            if (mPC >= mData.Length)
+            {
+                throw new InvalidDataException($"Invalid pc:{mPC}");
+            }
+            Int64 opcode = instruction % 100;
+            Int64 param1Mode = (instruction / 100) % 10;
+            Int64 param2Mode = (instruction / 1000) % 10;
+            Int64 param3Mode = (instruction / 10000) % 10;
+
+            if ((param1Mode != 0) && (param1Mode != 1) && (param1Mode != 2))
+            {
+                throw new ArgumentOutOfRangeException("param1Mode", $"Invalid param1Mode:{param1Mode}");
+            }
+            if ((param2Mode != 0) && (param2Mode != 1) && (param2Mode != 2))
+            {
+                throw new ArgumentOutOfRangeException("param2Mode", $"Invalid param1Mode:{param2Mode}");
+            }
+            if ((param3Mode != 0) && (param3Mode != 2))
+            {
+                throw new ArgumentOutOfRangeException("param3Mode", $"Invalid param3Mode:{param3Mode}");
+            }
+
+            if (opcode == 1)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param3Index = mData[mPC + 3];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                Int64 param3 = GetOutputIndex(param3Mode, param3Index);
+                Int64 output = param1 + param2;
+                MakeDataBigEnough(param3);
+                mData[param3] = output;
+                mPC += 4;
+            }
+            else if (opcode == 2)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param3Index = mData[mPC + 3];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                Int64 param3 = GetOutputIndex(param3Mode, param3Index);
+                Int64 output = param1 * param2;
+                MakeDataBigEnough(param3);
+                mData[param3] = output;
+                mPC += 4;
+            }
+            else if (opcode == 3)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 index = GetOutputIndex(param1Mode, param1Index);
+                MakeDataBigEnough(index);
+                mData[index] = input;
+                mPC += 2;
+            }
+            else if (opcode == 4)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                result = param1;
+                hasOutput = true;
+                mPC += 2;
+                return result;
+            }
+            else if (opcode == 5)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                if (param1 != 0)
+                {
+                    mPC = param2;
+                }
+                else
+                {
+                    mPC += 3;
+                }
+            }
+            else if (opcode == 6)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                if (param1 == 0)
+                {
+                    mPC = param2;
+                }
+                else
+                {
+                    mPC += 3;
+                }
+            }
+            else if (opcode == 7)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param3Index = mData[mPC + 3];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                Int64 param3 = GetOutputIndex(param3Mode, param3Index);
+                Int64 output = 0;
+                if (param1 < param2)
+                {
+                    output = 1;
+                }
+                MakeDataBigEnough(param3);
+                mData[param3] = output;
+                mPC += 4;
+            }
+            else if (opcode == 8)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param2Index = mData[mPC + 2];
+                Int64 param3Index = mData[mPC + 3];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                Int64 param2 = GetParam(param2Mode, param2Index);
+                Int64 param3 = GetOutputIndex(param3Mode, param3Index);
+                Int64 output = 0;
+                if (param1 == param2)
+                {
+                    output = 1;
+                }
+                MakeDataBigEnough(param3);
+                mData[param3] = output;
+                mPC += 4;
+            }
+            else if (opcode == 9)
+            {
+                Int64 param1Index = mData[mPC + 1];
+                Int64 param1 = GetParam(param1Mode, param1Index);
+                mRelativeBase += param1;
+                mPC += 2;
+            }
+            else
+            {
+                throw new InvalidDataException($"Unknown opcode:{opcode}");
+            }
+            instruction = mData[mPC];
+        }
+        halt = true;
+        return result;
+    }
+
+    Int64 GetParam(Int64 paramMode, Int64 paramIndex)
+    {
+        if (paramMode == 1)
+        {
+            return paramIndex;
+        }
+        Int64 index;
+        if (paramMode == 0)
+        {
+            index = paramIndex;
+        }
+        else if (paramMode == 2)
+        {
+            index = paramIndex + mRelativeBase;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("paramMode", $"Invalid paramMode {paramMode}");
+        }
+        MakeDataBigEnough(index);
+        return mData[index];
+    }
+
+    void MakeDataBigEnough(Int64 index)
+    {
+        if (index < 0)
+        {
+            throw new InvalidDataException($"Invalid parameter index {index}");
+        }
+        if (index >= mData.Length)
+        {
+            Int64[] newData = new Int64[index + 1];
+            int i = 0;
+            foreach (var d in mData)
+            {
+                newData[i] = d;
+                ++i;
+            }
+            for (; i < index; ++i)
+            {
+                newData[i] = 0;
+            }
+            mData = newData;
+        }
+    }
+
+    Int64 GetOutputIndex(Int64 paramMode, Int64 paramIndex)
+    {
+        if (paramMode == 0)
+        {
+            return paramIndex;
+        }
+        else if (paramMode == 2)
+        {
+            return paramIndex + mRelativeBase;
+        }
+        else
+        {
+            throw new InvalidDataException($"Invalid input paramMode {paramMode}");
+        }
+    }
+
+}
