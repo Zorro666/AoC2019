e81a3dae609fe6fba6d3eac7517bff66a9cc643c
Jake Turner <jake@unity3d.com>
2020-01-01 16:53:23 +0000

    Part1 completed, starting part2

--- AoC2019/Day16/Day16.cs
+++ AoC2019/Day16/Day16.cs
@@ -71,21 +71,100 @@ Here are the first eight digits of the final output list after 100 phases for so
 19617804207202209144916044189917 becomes 73745418.
 69317163492948606335995924319873 becomes 52432133.
 After 100 phases of FFT, what are the first eight digits in the final output list?
+
+--- Part Two ---
+
+Now that your FFT is working, you can decode the real signal.
+
+The real signal is your puzzle input repeated 10000 times. Treat this new signal as a single input list. Patterns are still calculated as before, and 100 phases of FFT are still applied.
+
+The first seven digits of your initial input signal also represent the message offset. The message offset is the location of the eight-digit message in the final output list. Specifically, the message offset indicates the number of digits to skip before reading the eight-digit message. For example, if the first seven digits of your initial input signal were 1234567, the eight-digit message would be the eight digits after skipping 1,234,567 digits of the final output list. Or, if the message offset were 7 and your final output list were 98765432109876543210, the eight-digit message would be 21098765. (Of course, your real message offset will be a seven-digit number, not a one-digit number like 7.)
+
+Here is the eight-digit message in the final output list after 100 phases. The message offset given in each input has been highlighted. (Note that the inputs given below are repeated 10000 times to find the actual starting input lists.)
+
+03036732577212944063491565474664 becomes 84462026.
+02935109699940807407585447034323 becomes 78725270.
+03081770884921959731165446850517 becomes 53553731.
+
+After repeating your input signal 10000 times and running 100 phases of FFT, what is the eight-digit message embedded in the final output list?
+
 */
 
 namespace Day16
 {
     class Program
     {
+        static readonly int[] sPatternLookup = { 0, 1, 0, -1 };
+
         private Program(string inputFile, bool part1)
         {
-            var elements = ReadProgram(inputFile);
+            var start = ReadProgram(inputFile);
+            if (part1)
+            {
+                var result = RunFFT(start, 100);
+                Console.WriteLine($"Day16 part1:{result}");
+            }
+            else
+            {
+                var result = RunFFT(start, 200);
+                Console.WriteLine($"Day16 part2:{result}");
+            }
         }
 
-        private string[] ReadProgram(string inputFile)
+        private string ReadProgram(string inputFile)
         {
-            var elements = File.ReadAllLines(inputFile);
+            var elements = File.ReadAllText(inputFile);
             return elements;
+        }
+
+        public static string RunFFT(string start, int numIterations)
+        {
+            var numDigits = start.Length;
+            var startDigits = new int[numDigits];
+            var currentDigits = new int[numDigits];
+            var nextDigits = new int[numDigits];
+            for (int d = 0; d < numDigits; ++d)
+            {
+                char charDigit = start[d];
+                startDigits[d] = (int)charDigit - '0';
+                currentDigits[d] = startDigits[d];
+                nextDigits[d] = startDigits[d];
+            }
+
+            for (int i = 0; i < numIterations; ++i)
+            {
+                for (int d = 0; d < numDigits; ++d)
+                {
+                    currentDigits[d] = nextDigits[d];
+                }
+                for (int outputDigit = 0; outputDigit < numDigits; ++outputDigit)
+                {
+                    long total = 0;
+                    int repeat = outputDigit + 1;
+                    for (int d = outputDigit; d < numDigits; ++d)
+                    {
+                        /*
+                        The base pattern is 0, 1, 0, -1.Then, repeat each value in the pattern a number of times equal to the position in the output list being considered.Repeat once for the first element, twice for the second element, three times for the third element, and so on.So, if the third element of the output list is being calculated, repeating the values would produce: 0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1.
+                        */
+                        // d =0 : 1, 0, -1, 0, 1, 0, -1
+                        // d =1 : 0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1
+                        // d =2 : 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, 0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1
+                        int patternIndex = ((d + 1) / repeat) % 4;
+                        int pattern = sPatternLookup[patternIndex];
+                        total += currentDigits[d] * pattern;
+                        //Console.WriteLine($"out:{outputDigit} d:{d} pattern:{pattern} patternIndex:{patternIndex} total:{total}");
+                    }
+                    nextDigits[outputDigit] = (int)(Math.Abs(total) % 10);
+                }
+            }
+
+            var result = new char[8];
+            for (int d = 0; d < 8; ++d)
+            {
+                result[d] = (char)(nextDigits[d] + '0');
+
+            }
+            return new string(result);
         }
 
         public static void Run()
--- AoC2019/Day16/Day16Tests.cs
+++ AoC2019/Day16/Day16Tests.cs
@@ -6,9 +6,22 @@ namespace Day16
     public class Tests
     {
         [Test]
-        public void Day11Tests(string parent, string node)
+        [TestCase("12345678", 1, ExpectedResult = "48226158")]
+        [TestCase("12345678", 2, ExpectedResult = "34040438")]
+        [TestCase("12345678", 3, ExpectedResult = "03415518")]
+        [TestCase("12345678", 4, ExpectedResult = "01029498")]
+        [TestCase("48226158", 1, ExpectedResult = "34040438")]
+        [TestCase("48226158", 2, ExpectedResult = "03415518")]
+        [TestCase("48226158", 3, ExpectedResult = "01029498")]
+        [TestCase("34040438", 1, ExpectedResult = "03415518")]
+        [TestCase("34040438", 2, ExpectedResult = "01029498")]
+        [TestCase("03415518", 1, ExpectedResult = "01029498")]
+        [TestCase("80871224585914546619083218645595", 100, ExpectedResult = "24176176")]
+        [TestCase("19617804207202209144916044189917", 100, ExpectedResult = "73745418")]
+        [TestCase("69317163492948606335995924319873", 100, ExpectedResult = "52432133")]
+        public string RunFFT(string start, int numIterations)
         {
-            Assert.Fail();
+            return Program.RunFFT(start, numIterations);
         }
     }
 }
