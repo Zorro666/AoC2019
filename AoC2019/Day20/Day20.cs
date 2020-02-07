8f8ce1cf3b520c890e875d4ebbf54a9223261a90
Jake Turner <jake@unity3d.com>
2020-02-07 09:20:00 +0000

    Safety commit of Day20 work : map parsing in progress

--- AoC2019/Day20/Day20.cs
+++ AoC2019/Day20/Day20.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 /*
@@ -80,19 +81,373 @@ Here, AA has no direct path to ZZ, but it does connect to AS and CP. By passing 
 In your maze, how many steps does it take to get from the open tile marked AA to the open tile marked ZZ?
 
 */
+
 namespace Day20
 {
+    struct Node
+    {
+        public int x;
+        public int y;
+        public int type;
+    };
+
     class Program
     {
+        static readonly int PORTAL_START = 10;
+        static int[,] sMap;
+        static (int w, int h) sMapSize;
+        static int sStartNode;
+        static int sExitNode;
+        static Node[] sNodes;
+        static List<int>[] sLinks;
+
         private Program(string inputFile, bool part1)
         {
-            var elements = ReadProgram(inputFile);
+            var mapSource = ReadProgram(inputFile);
+            if (part1)
+            {
+                ParseMap(mapSource, false);
+                OutputMap(false);
+            }
         }
 
         private string[] ReadProgram(string inputFile)
         {
-            var elements = File.ReadAllLines(inputFile);
-            return elements;
+            var mapSource = File.ReadAllLines(inputFile);
+            return mapSource;
+        }
+
+        public static void ParseMap(string[] mapSource, bool applyPart2Setup)
+        {
+            if (mapSource.Length == 0)
+            {
+                throw new ArgumentException($"map input is empty", nameof(mapSource));
+            }
+            sMapSize.w = mapSource[0].Length;
+            sMapSize.h = 0;
+            foreach (var line in mapSource)
+            {
+                if (line.Length != sMapSize.w)
+                {
+                    throw new ArgumentException($"map input lines must be the same length {sMapSize.w} {line.Length}", nameof(mapSource));
+                }
+                sMapSize.h++;
+            }
+            sMap = new int[sMapSize.w, sMapSize.h];
+            sNodes = new Node[sMapSize.w * sMapSize.h];
+            sLinks = new List<int>[sMapSize.w * sMapSize.h];
+            sStartNode = -1;
+            sExitNode = -1;
+            var portalCells = new List<(int id, int x, int y)>(100);
+            for (int y = 0; y < sMapSize.h; ++y)
+            {
+                var line = mapSource[y];
+                for (int x = 0; x < sMapSize.w; ++x)
+                {
+                    var nodeIndex = GetNodeIndex(x, y);
+                    char cell = line[x];
+                    if (cell == ' ')
+                    {
+                        sMap[x, y] = -1;
+                    }
+                    else if (cell == '.')
+                    {
+                        sMap[x, y] = 0;
+                    }
+                    else if (cell == '#')
+                    {
+                        sMap[x, y] = 1;
+                    }
+                    else if ((cell >= 'A') && (cell <= 'Z'))
+                    {
+                        int portalId = PORTAL_START + (cell - 'A');
+                        sMap[x, y] = portalId;
+                        portalCells.Add((portalId, x, y));
+                    }
+                    else
+                    {
+                        throw new InvalidDataException($"Unknown input map[{x},{y}] {cell}");
+                    }
+                    sNodes[nodeIndex].x = x;
+                    sNodes[nodeIndex].y = y;
+                    sNodes[nodeIndex].type = sMap[x, y];
+                }
+            }
+
+            // Parse the portals and connect them together
+            // Find the other portal letter, entry/exit point, is the map start or end
+            var portals = new List<(int id0, int id1, int entryExitX, int entryExitY)>(50);
+            foreach (var portal in portalCells)
+            {
+                // ? 
+                //?P?
+                // ? 
+                int pId = portal.id;
+                int pX = portal.x;
+                int pY = portal.y;
+                int countValidPortalId = 0;
+                (int id, int x, int y) validPortal = (-1, -1, -1);
+
+                var tryTargets = new (int x, int y)[] { (pX, pY - 1), (pX - 1, pY), (pX + 1, pY), (pX, pY + 1) };
+
+                foreach ((int tryX, int tryY) in tryTargets)
+                {
+                    int targetId = GetMapCell(tryX, tryY);
+                    if (targetId >= PORTAL_START)
+                    {
+                        ++countValidPortalId;
+                        validPortal.id = targetId;
+                        validPortal.x = tryX;
+                        validPortal.y = tryY;
+                    }
+                }
+                if (countValidPortalId != 1)
+                {
+                    throw new InvalidDataException($"Invalid portal at {(char)(pId - PORTAL_START + 'A')} {pX},{pY} wrong number of portalLetters {countValidPortalId}");
+                }
+
+                // Find the entry/exit point for this portal instance
+                var startPoints = new (int x, int y)[] { (pX, pY), (validPortal.x, validPortal.y) };
+                int countValidEntryExitPoints = 0;
+                (int x, int y) entryExit = (-1, -1);
+                foreach ((int sX, int sY) in startPoints)
+                {
+                    tryTargets = new (int x, int y)[] { (sX, sY - 1), (sX - 1, sY), (sX + 1, sY), (sX, sY + 1) };
+                    foreach ((int tryX, int tryY) in tryTargets)
+                    {
+                        int targetId = GetMapCell(tryX, tryY);
+                        if (targetId == 0)
+                        {
+                            ++countValidEntryExitPoints;
+                            entryExit.x = tryX;
+                            entryExit.y = tryY;
+                        }
+                    }
+                }
+                if (countValidEntryExitPoints != 1)
+                {
+                    throw new InvalidDataException($"Invalid portal at {(char)(pId - PORTAL_START + 'A')} {pX},{pY} wrong number of entry exit points {countValidEntryExitPoints}");
+                }
+                portals.Add((pId, validPortal.id, entryExit.x, entryExit.y));
+            }
+            // Find the start and end portal instances
+            foreach (var portal in portals)
+            {
+                if (portal.id0 == portal.id1)
+                {
+                    if (portal.id0 == PORTAL_START + 'A' - 'A')
+                    {
+                        sStartNode = GetNodeIndex(portal.entryExitX, portal.entryExitY);
+                    }
+                    else if (portal.id0 == PORTAL_START + 'Z' - 'A')
+                    {
+                        sExitNode = GetNodeIndex(portal.entryExitX, portal.entryExitY);
+                    }
+                    else
+                    {
+                        throw new InvalidDataException($"Invalid portal type {(char)(portal.id0 - PORTAL_START + 'A')}, {(char)(portal.id1 - PORTAL_START + 'A')} at {portal.entryExitX},{portal.entryExitY}");
+                    }
+                }
+            }
+            // Find the connected portal to make : PortalId: enterIndex exitIndex
+
+            if (sStartNode == -1)
+            {
+                throw new InvalidDataException($"Failed to find the start point {sStartNode}");
+            }
+
+            if (sExitNode == -1)
+            {
+                throw new InvalidDataException($"Failed to find the exit point {sExitNode}");
+            }
+
+            for (int y = 0; y < sMapSize.h; ++y)
+            {
+                for (int x = 0; x < sMapSize.w; ++x)
+                {
+                    var nodeIndex = x + y * sMapSize.w;
+                    int cell = sMap[x, y];
+                    sLinks[nodeIndex] = new List<int>();
+                    sLinks[nodeIndex].Clear();
+                    if (cell == -1)
+                    {
+                        continue;
+                    }
+                    if (cell == 1)
+                    {
+                        continue;
+                    }
+                    if ((x - 1 >= 0) && (sMap[x - 1, y] != 1))
+                    {
+                        var targetNodeIndex = x - 1 + y * sMapSize.w;
+                        sLinks[nodeIndex].Add(targetNodeIndex);
+                    }
+                    if ((x + 1 < sMapSize.w) && (sMap[x + 1, y] != 1))
+                    {
+                        var targetNodeIndex = x + 1 + y * sMapSize.w;
+                        sLinks[nodeIndex].Add(targetNodeIndex);
+                    }
+                    if ((y - 1 >= 0) && (sMap[x, y - 1] != 1))
+                    {
+                        var targetNodeIndex = x + (y - 1) * sMapSize.w;
+                        sLinks[nodeIndex].Add(targetNodeIndex);
+                    }
+                    if ((y + 1 < sMapSize.h) && (sMap[x, y + 1] != 1))
+                    {
+                        var targetNodeIndex = x + (y + 1) * sMapSize.w;
+                        sLinks[nodeIndex].Add(targetNodeIndex);
+                    }
+                }
+            }
+        }
+
+        public static void OutputMap(bool detailed)
+        {
+            for (int y = 0; y < sMapSize.h; ++y)
+            {
+                string line = "";
+                for (int x = 0; x < sMapSize.w; ++x)
+                {
+                    int cell = sMap[x, y];
+                    if (cell == -1)
+                    {
+                        line += ' ';
+                    }
+                    else if (cell == 0)
+                    {
+                        line += '.';
+                    }
+                    else if (cell == 1)
+                    {
+                        line += '#';
+                    }
+                    else if (cell >= PORTAL_START)
+                    {
+                        line += (char)('A' + (cell - PORTAL_START));
+                    }
+                    else
+                    {
+                        throw new InvalidDataException($"Unknown map[{x},{y}] {cell}");
+                    }
+                }
+                Console.WriteLine($"{line}");
+            }
+
+            if (detailed)
+            {
+                for (int nodeIndex = 0; nodeIndex < sNodes.Length; ++nodeIndex)
+                {
+                    var node = sNodes[nodeIndex];
+                    Console.WriteLine($"Node:{node.x},{node.y} {node.type}");
+                    foreach (var linkTargetIndex in sLinks[nodeIndex])
+                    {
+                        (int linkX, int linkY) = GetXYFromNodeIndex(linkTargetIndex);
+                        Console.WriteLine($"Link:{linkTargetIndex} {linkX},{linkY}");
+                    }
+                }
+            }
+        }
+
+        static int GetNodeIndex(int x, int y)
+        {
+            if ((x < 0) || (x >= sMapSize.w))
+            {
+                throw new ArgumentOutOfRangeException("x", $"Invalid value {x} out of range 0-{sMapSize.w}");
+            }
+            if ((y < 0) || (y >= sMapSize.h))
+            {
+                throw new ArgumentOutOfRangeException("y", $"Invalid value {y} out of range 0-{sMapSize.h}");
+            }
+            return x + y * sMapSize.w;
+        }
+
+        static (int, int) GetXYFromNodeIndex(int nodeIndex)
+        {
+            int x = nodeIndex % sMapSize.w;
+            int y = nodeIndex / sMapSize.w;
+            return (x, y);
+        }
+
+        static int GetMapCell(int x, int y)
+        {
+            if ((x >= 0) && (x < sMapSize.w) && (y >= 0) && (y < sMapSize.h))
+            {
+                return sMap[x, y];
+            }
+            return -666;
+        }
+
+        public static int ShortestPath()
+        {
+            return ShortestPath(sStartNode, sExitNode);
+        }
+
+        static int ShortestPath(int startIndex, int endIndex)
+        {
+            //(int fromX, int fromY) = GetXYFromNodeIndex(startIndex);
+            //Console.WriteLine($"ShortestPath Start {startIndex} {fromX},{fromY}");
+            Queue<int> nodesToVisit = new Queue<int>();
+            nodesToVisit.Enqueue(startIndex);
+            bool[] visited = new bool[sNodes.Length];
+            int[] parents = new int[sNodes.Length];
+            int minNumSteps = int.MaxValue;
+            for (int i = 0; i < parents.Length; ++i)
+            {
+                parents[i] = -1;
+            }
+            while (nodesToVisit.Count > 0)
+            {
+                int nodeIndex = nodesToVisit.Dequeue();
+                (int x, int y) = GetXYFromNodeIndex(nodeIndex);
+                //Console.WriteLine($"Node:{nodeIndex} {x},{y}");
+                if (nodeIndex == endIndex)
+                {
+                    //Console.WriteLine($"Solved");
+                    int numSteps = 0;
+                    int currentNodeIndex = endIndex;
+                    while (currentNodeIndex != -1)
+                    {
+                        (x, y) = GetXYFromNodeIndex(currentNodeIndex);
+                        //Console.WriteLine($"Node:{currentNodeIndex} {x},{y}");
+                        numSteps++;
+                        currentNodeIndex = parents[currentNodeIndex];
+                        if (currentNodeIndex == startIndex)
+                        {
+                            break;
+                        }
+                    }
+                    //Console.WriteLine($"numSteps:{numSteps}");
+                    if (numSteps < minNumSteps)
+                    {
+                        minNumSteps = numSteps;
+                    }
+                }
+
+                foreach (var link in sLinks[nodeIndex])
+                {
+                    if (visited[link] == false)
+                    {
+                        int linkType = sNodes[link].type;
+                        bool validLink = true;
+                        if (link == endIndex)
+                        {
+                            validLink = true;
+                        }
+                        if (validLink)
+                        {
+                            visited[link] = true;
+                            nodesToVisit.Enqueue(link);
+                            parents[link] = nodeIndex;
+                        }
+                    }
+                }
+            };
+
+            if (minNumSteps < int.MaxValue)
+            {
+                return minNumSteps;
+            }
+            return -1;
         }
 
         public static void Run()
--- AoC2019/Day20/Day20Tests.cs
+++ AoC2019/Day20/Day20Tests.cs
@@ -1,14 +1,87 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace Day20
 {
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Day11Tests(string parent, string node)
+        private static readonly string[] mapA = {
+"         A         ",
+"         A         ",
+"  #######.#########",
+"  #######.........#",
+"  #######.#######.#",
+"  #######.#######.#",
+"  #######.#######.#",
+"  #####  B    ###.#",
+"BC...##  C    ###.#",
+"  ##.##       ###.#",
+"  ##...DE  F  ###.#",
+"  #####    G  ###.#",
+"  #########.#####.#",
+"DE..#######...###.#",
+"  #.#########.###.#",
+"FG..#########.....#",
+"  ###########.#####",
+"             Z     ",
+"             Z     "
+        };
+
+        private static readonly string[] mapB = {
+"                   A               ",
+"                   A               ",
+"  #################.#############  ",
+"  #.#...#...................#.#.#  ",
+"  #.#.#.###.###.###.#########.#.#  ",
+"  #.#.#.......#...#.....#.#.#...#  ",
+"  #.#########.###.#####.#.#.###.#  ",
+"  #.............#.#.....#.......#  ",
+"  ###.###########.###.#####.#.#.#  ",
+"  #.....#        A   C    #.#.#.#  ",
+"  #######        S   P    #####.#  ",
+"  #.#...#                 #......VT",
+"  #.#.#.#                 #.#####  ",
+"  #...#.#               YN....#.#  ",
+"  #.###.#                 #####.#  ",
+"DI....#.#                 #.....#  ",
+"  #####.#                 #.###.#  ",
+"ZZ......#               QG....#..AS",
+"  ###.###                 #######  ",
+"JO..#.#.#                 #.....#  ",
+"  #.#.#.#                 ###.#.#  ",
+"  #...#..DI             BU....#..LF",
+"  #####.#                 #.#####  ",
+"YN......#               VT..#....QG",
+"  #.###.#                 #.###.#  ",
+"  #.#...#                 #.....#  ",
+"  ###.###    J L     J    #.#.###  ",
+"  #.....#    O F     P    #.#...#  ",
+"  #.###.#####.#.#####.#####.###.#  ",
+"  #...#.#.#...#.....#.....#.#...#  ",
+"  #.#####.###.###.#.#.#########.#  ",
+"  #...#.#.....#...#.#.#.#.....#.#  ",
+"  #.###.#####.###.###.#.#.#######  ",
+"  #.#.........#...#.............#  ",
+"  #########.###.###.#############  ",
+"           B   J   C               ",
+"           U   P   P               "
+        };
+
+        public static IEnumerable<TestCaseData> ShortestPathCases => new[]
         {
-            Assert.Fail();
+            new TestCaseData(mapA, 23).SetName("ShorttestPath A 23"),
+            new TestCaseData(mapB, 58).SetName("ShorttestPath B 58"),
+        };
+
+        [Test]
+        [TestCaseSource("ShortestPathCases")]
+        public void ShortestPath(string[] map, int expectedNumSteps)
+        {
+            Program.ParseMap(map, false);
+            Program.OutputMap(false);
+            Assert.That(Program.ShortestPath(), Is.EqualTo(expectedNumSteps));
         }
     }
 }
+
