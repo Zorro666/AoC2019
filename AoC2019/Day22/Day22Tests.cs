8effde33a75c05e2fef8506ff06ee9f4abe39c8a
Jake Turner <jake@unity3d.com>
2020-02-13 18:26:56 +0000

    Tests on a deck of size 11 instead of 10

--- AoC2019/Day22/Day22Tests.cs
+++ AoC2019/Day22/Day22Tests.cs
@@ -23,8 +23,8 @@ namespace Day22
 "deal with increment 3",
         };
 
-        private static readonly string[] DealWithIncrementEleven = {
-"deal with increment 11",
+        private static readonly string[] DealWithIncrementTwelve = {
+"deal with increment 12",
         };
 
         private static readonly string[] CutZero = {
@@ -92,39 +92,39 @@ namespace Day22
 
         public static IEnumerable<TestCaseData> ShuffleTestCases => new[]
         {
-            new TestCaseData(null, "0 1 2 3 4 5 6 7 8 9").SetName("StartingDeck"),
+            new TestCaseData(null, "0 1 2 3 4 5 6 7 8 9 10").SetName("StartingDeck"),
 
-            new TestCaseData(DealNewStack, "9 8 7 6 5 4 3 2 1 0").SetName(nameof(DealNewStack)),
-            new TestCaseData(DealNewStackTwice, "0 1 2 3 4 5 6 7 8 9").SetName(nameof(DealNewStackTwice)),
+            new TestCaseData(DealNewStack, "10 9 8 7 6 5 4 3 2 1 0").SetName(nameof(DealNewStack)),
+            new TestCaseData(DealNewStackTwice, "0 1 2 3 4 5 6 7 8 9 10").SetName(nameof(DealNewStackTwice)),
 
-            new TestCaseData(DealWithIncrementOne, "0 1 2 3 4 5 6 7 8 9").SetName(nameof(DealWithIncrementOne)),
-            new TestCaseData(DealWithIncrementThree, "0 7 4 1 8 5 2 9 6 3").SetName(nameof(DealWithIncrementThree)),
-            new TestCaseData(DealWithIncrementEleven, "0 1 2 3 4 5 6 7 8 9").SetName(nameof(DealWithIncrementEleven)),
+            new TestCaseData(DealWithIncrementOne, "0 1 2 3 4 5 6 7 8 9 10").SetName(nameof(DealWithIncrementOne)),
+            new TestCaseData(DealWithIncrementThree, "0 4 8 1 5 9 2 6 10 3 7").SetName(nameof(DealWithIncrementThree)),
+            new TestCaseData(DealWithIncrementTwelve, "0 1 2 3 4 5 6 7 8 9 10").SetName(nameof(DealWithIncrementTwelve)),
 
-            new TestCaseData(CutZero, "0 1 2 3 4 5 6 7 8 9").SetName(nameof(CutZero)),
-            new TestCaseData(CutOne, "1 2 3 4 5 6 7 8 9 0").SetName(nameof(CutOne)),
-            new TestCaseData(CutThree, "3 4 5 6 7 8 9 0 1 2").SetName(nameof(CutThree)),
-            new TestCaseData(CutFive, "5 6 7 8 9 0 1 2 3 4").SetName(nameof(CutFive)),
-            new TestCaseData(CutSix, "6 7 8 9 0 1 2 3 4 5").SetName(nameof(CutSix)),
-            new TestCaseData(CutMinusOne, "9 0 1 2 3 4 5 6 7 8").SetName(nameof(CutMinusOne)),
-            new TestCaseData(CutMinusFour, "6 7 8 9 0 1 2 3 4 5").SetName(nameof(CutMinusFour)),
-            new TestCaseData(CutMinusFive, "5 6 7 8 9 0 1 2 3 4").SetName(nameof(CutMinusFive)),
+            new TestCaseData(CutZero, "0 1 2 3 4 5 6 7 8 9 10").SetName(nameof(CutZero)),
+            new TestCaseData(CutOne, "1 2 3 4 5 6 7 8 9 10 0").SetName(nameof(CutOne)),
+            new TestCaseData(CutThree, "3 4 5 6 7 8 9 10 0 1 2").SetName(nameof(CutThree)),
+            new TestCaseData(CutFive, "5 6 7 8 9 10 0 1 2 3 4").SetName(nameof(CutFive)),
+            new TestCaseData(CutSix, "6 7 8 9 10 0 1 2 3 4 5").SetName(nameof(CutSix)),
+            new TestCaseData(CutMinusOne, "10 0 1 2 3 4 5 6 7 8 9").SetName(nameof(CutMinusOne)),
+            new TestCaseData(CutMinusFour, "7 8 9 10 0 1 2 3 4 5 6").SetName(nameof(CutMinusFour)),
+            new TestCaseData(CutMinusFive, "6 7 8 9 10 0 1 2 3 4 5").SetName(nameof(CutMinusFive)),
 
-            new TestCaseData(ShuffleA, "0 3 6 9 2 5 8 1 4 7").SetName(nameof(ShuffleA)),
-            new TestCaseData(ShuffleB, "3 0 7 4 1 8 5 2 9 6").SetName(nameof(ShuffleB)),
-            new TestCaseData(ShuffleC, "6 3 0 7 4 1 8 5 2 9").SetName(nameof(ShuffleC)),
-            new TestCaseData(ShuffleD, "9 2 5 8 1 4 7 0 3 6").SetName(nameof(ShuffleD))
+            new TestCaseData(ShuffleA, "0 8 5 2 10 7 4 1 9 6 3").SetName(nameof(ShuffleA)),
+            new TestCaseData(ShuffleB, "9 1 4 7 10 2 5 8 0 3 6").SetName(nameof(ShuffleB)),
+            new TestCaseData(ShuffleC, "8 4 0 7 3 10 6 2 9 5 1").SetName(nameof(ShuffleC)),
+            new TestCaseData(ShuffleD, "1 8 4 0 7 3 10 6 2 9 5").SetName(nameof(ShuffleD))
         };
 
         [Test]
         [TestCaseSource("ShuffleTestCases")]
         public void ShuffleTests(string[] instructions, string expectedResult)
         {
-            Program.CreateDeck(10);
+            Program.CreateDeck(11);
             Program.RunInstructions(instructions);
             Assert.That(Program.DeckAsString(), Is.EqualTo(expectedResult));
 
-            Program.CreateDeck(10);
+            Program.CreateDeck(11);
             Program.ApplyInstructionsUsingEquation(instructions);
             Assert.That(Program.DeckAsString(), Is.EqualTo(expectedResult));
         }
